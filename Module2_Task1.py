34368ff3326a34d4b867798e709e50a71251fd52
viskrishnan <krishnansubramanian007@gmail.com>
2025-07-26 09:10:33 +0530

    ASSIGNMENT 1 Module 2: Basic Python Concepts

---
+++ Module2_Task1.py
@@ -0,0 +1,20 @@
+# Task 1: Perform Basic Mathematical Operations
+
+firstNum = input("Enter the first number: ")
+firstNum = int(firstNum)
+secNum = input("Enter the second number: ")
+secNum = int(secNum)
+
+Add = firstNum + secNum
+Sub = secNum - firstNum
+Mul = firstNum * secNum
+Div = secNum / firstNum
+Add = int(Add)
+Sub = int(Sub)
+Mul = int(Mul)
+Div = float(Div)
+
+print("Addition: ", Add)
+print("Subtraction: ", Sub)
+print("Multiplication: ",Mul)
+print("Division: ",Div)
---
+++ Module2_Task2_Greeting.py
@@ -0,0 +1,8 @@
+# Task 2: Create a Personalized Greeting
+
+FirstName = input("Enter the First name: ")
+FirstName = str(FirstName)
+LastName = input("Enter the Last Name: ")
+LastName = str(LastName)
+
+print("Hello,", FirstName , LastName+"!","Welcome to the Python Program")
