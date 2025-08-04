e16704052481977cf465608d4985476a431546ea
viskrishnan <krishnansubramanian007@gmail.com>
2025-08-04 20:44:11 +0530

    Optimized the project folder structure

---
+++ Experiments/LearningDictionaries.py
@@ -0,0 +1,13 @@
+"""
+list --> [] index value
+list  = [1,2,3,4]
+
+dictionary --> {} , key
+dictionary --> {key:value, key:value......}
+"""
+
+my_dictionary = {'key1': 1, 'key2': 22, 'key3': 333, 'key4':4444, 'key5':55555}
+print(my_dictionary['key5'])
+
+mydict = {1:'a', 2:'b', 3:'c', 4:'d'}
+print(mydict)
